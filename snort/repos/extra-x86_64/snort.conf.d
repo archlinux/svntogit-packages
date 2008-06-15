919b595017485d11fc229ab768dcf4e441cb763f
andyrtr <andyrtr@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2008-06-15 18:17:31 +0000

    Merged revisions 324-2916 via svnmerge from svn+ssh://svn.archlinux.org/home/svn-packages/snort/trunk
    ........
      r356 | aaron | 2008-04-18 22:56:27 +0000 (Fr, 18 Apr 2008) | 1 line
      
      Added svn:keywords to all PKGBUILDs
    ........
      r2916 | andyrtr | 2008-06-15 18:16:57 +0000 (So, 15 Jun 2008) | 2 lines
      
      upgpkg: snort 2.8.2-1
          updatepkg + fix #10470
    ........
    
    
    git-svn-id: file:///srv/repos/svn-packages/svn@2917 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- snort/repos/extra-x86_64/snort.conf.d
+++ snort/repos/extra-x86_64/snort.conf.d
@@ -1,4 +1,18 @@
 #
 # Parameters to be passed to snort
 #
-SNORT_ARGS="-c /etc/snort -D"
+
+# options taken from Fedora
+# http://cvs.fedoraproject.org/viewcvs/devel/snort/sysconfig.snort?rev=1.2&view=markup
+
+# What user account should we run under.
+USER="root"
+
+# What group account should we run under. 
+GROUP="root"
+
+# define the interface we listen on
+INTERFACE="eth0"
+
+# If you are using prelude, delete the '-A fast' option
+SNORT_OPTIONS="-A fast -b -l /var/log/snort -D -p"
