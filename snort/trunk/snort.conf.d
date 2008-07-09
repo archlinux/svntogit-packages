425b53cf30a63ac1763c15f49d395c12f9777f49
hugo <hugo@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2008-07-09 13:40:57 +0000

    upgpkg: snort 2.8.2.1-3     snort updated! lots of bugs fixed. see FS#10775 and FS#10072 for details
    git-svn-id: file:///srv/repos/svn-packages/svn@4725 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- snort/trunk/snort.conf.d
+++ snort/trunk/snort.conf.d
@@ -5,11 +5,14 @@
 # options taken from Fedora
 # http://cvs.fedoraproject.org/viewcvs/devel/snort/sysconfig.snort?rev=1.2&view=markup
 
+# Where is the snort.conf file.
+SNORT_CONF="/etc/snort/snort.conf"
+
 # What user account should we run under.
-USER="root"
+USER="snort"
 
 # What group account should we run under. 
-GROUP="root"
+GROUP="snort"
 
 # define the interface we listen on
 INTERFACE="eth0"
