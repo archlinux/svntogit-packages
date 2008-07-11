ce362290485fe858ed2488f8cf96ba4476ec07ce
pierre <pierre@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2008-07-11 00:56:08 +0000

    upgpkg: lighttpd 1.4.19-2     keep it simple
    git-svn-id: file:///srv/repos/svn-packages/svn@4851 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- trunk/lighttpd.logrotate.d
+++ trunk/lighttpd.logrotate.d
@@ -1,8 +1,5 @@
 /var/log/lighttpd/*log {
-  create 644 nobody nobody
-  compress
-  postrotate
-    /bin/kill -HUP `cat /var/run/lighttpd.pid 2>/dev/null` 2> /dev/null || true
-  endscript
+   postrotate
+      /bin/kill -HUP `cat /var/run/lighttpd/lighttpd.pid 2>/dev/null` 2> /dev/null || true
+   endscript
 }
-
--- trunk/lighttpd.rc.d
+++ trunk/lighttpd.rc.d
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-# source application-specific settings
-[ -f /etc/conf.d/lighttpd ] && . /etc/conf.d/lighttpd
-
+# general config
 . /etc/rc.conf
 . /etc/rc.d/functions
 
-PID=`pidof -o %PPID ${DAEMON_NAME}`
+PID=`pidof -o %PPID /usr/sbin/lighttpd`
+
 case "$1" in
   start)
-    stat_busy "Starting ${DAEMON_NAME}"
-    [ -z "$PID" ] && ${DAEMON_PATH} -f ${DAEMON_CONF} >> $DAEMON_ERRLOG 2>&1
+    stat_busy "Starting lighttpd Daemon"
+    [ -z "$PID" ] && /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf >> /var/log/lighttpd/error.log 2>&1
     if [ $? -gt 0 ]; then
       stat_fail
     else
-      add_daemon ${DAEMON_NAME}
+      add_daemon lighttpd
       stat_done
     fi
     ;;
   stop)
-    stat_busy "Stopping ${DAEMON_NAME}"
+    stat_busy "Stopping lighttpd Daemon"
     [ ! -z "$PID" ] && kill $PID &>/dev/null
     if [ $? -gt 0 ]; then
       stat_fail
     else
-      rm_daemon ${DAEMON_NAME}
+      rm_daemon lighttpd
+      rm -f /var/run/lighttpd/lighttpd.pid
       stat_done
     fi
     ;;
   restart)
     $0 stop
-    sleep 1
     $0 start
     ;;
   *)
     echo "usage: $0 {start|stop|restart}"
 esac
-exit 0 
