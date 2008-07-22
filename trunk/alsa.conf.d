3a5108b4c38c5ab130f9fec2e871c1eaa30b0054
aaron <aaron@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2008-07-22 20:59:14 +0000

    Add updated daemon from FS#10003
    git-svn-id: file:///srv/repos/svn-packages/svn@6022 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- trunk/alsa.conf.d
+++ trunk/alsa.conf.d
@@ -1,4 +1,14 @@
+# Sound card state storage file, containing mixer settings.
+STATE_FILE=/etc/asound.state
+
 # Enables powersaving mode for AC97 and hda_intel audio chips.
 # Set to 1 to enable powersaving.
 # Set to 0 to disable powersaving (default).
 POWERSAVE=0
+
+# Whether to save volume levels when stopped ("yes" or "no").
+SAVE_VOLUME="yes"
+
+# Whether to mute the master volume when stopped ("yes" or "no").
+# Useful for bad audio cards which make a noise on system poweroff.
+MUTE_VOLUME="no"
