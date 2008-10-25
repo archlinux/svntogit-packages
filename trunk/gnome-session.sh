e73db6cf9eff7c51a1f2d2ec1fc2b7508f25ecce
jgc <jgc@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2008-10-25 10:17:08 +0000

    upgpkg: gnome-session 2.24.1-1
    git-svn-id: file:///srv/repos/svn-packages/svn@17128 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- trunk/gnome-session.sh
+++ trunk/gnome-session.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-. /etc/X11/xinit/xinitrc.d/30-dbus
-exec /usr/lib/gnome-session/gnome-session
+if [ -n "${DISPLAY}" ]; then
+  . /etc/X11/xinit/xinitrc.d/30-dbus
+fi
+exec /usr/lib/gnome-session/gnome-session $@
