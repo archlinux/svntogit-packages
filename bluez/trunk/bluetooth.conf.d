3a7de0bc7bf09cd8ec4051bc587d52aa1ac94398
geoffroy <geoffroy@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2009-02-12 01:07:57 +0000

    upgpkg: bluez 4.29-1
    git-svn-id: file:///srv/repos/svn-packages/svn@26769 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- bluez/trunk/bluetooth.conf.d
+++ bluez/trunk/bluetooth.conf.d
@@ -3,26 +3,26 @@
 # Bluetooth services (allowed values are "true" and "false")
 
 # Run the bluetoothd daemon (default: true)
-#DAEMON_ENABLE="true"
+#DAEMON_ENABLE="false"
 
 # Run hid2hci (default: false)
-#HID2HCI_ENABLE="false"
+#HID2HCI_ENABLE="true"
 
 # Run the sdp daemon (default: false)
 # If this is disabled, hcid's internal sdp daemon will be used
-#SDPD_ENABLE="false"
+#SDPD_ENABLE="true"
 
 # Run the bluetooth HID daemon (default: false)
-#HIDD_ENABLE="false"
+#HIDD_ENABLE="true"
 
 # Activate rfcomm ports (default: false)
-#RFCOMM_ENABLE="false"
+#RFCOMM_ENABLE="true"
 
 # Run bluetooth dial-up networking daemon (default: false)
-#DUND_ENABLE="false"
+#DUND_ENABLE="true"
 
 # Run bluetooth PAN daemon (default: false)
-#PAND_ENABLE="false"
+#PAND_ENABLE="true"
 
 # rfcomm configuration file (default: /etc/bluetooth/rfcomm.conf)
 #RFCOMM_CONFIG="/etc/bluetooth/rfcomm.conf"
