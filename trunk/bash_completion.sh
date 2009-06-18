d9ef762337dade44df417802126be7c9f3ff39b1
aaron <aaron@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2009-06-18 17:08:34 +0000

    upgpkg: bash-completion 1.0-1     Revamp of bash-completion See FS#11196 and FS#11195
    git-svn-id: file:///srv/repos/svn-packages/svn@42711 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- trunk/bash_completion.sh
+++ trunk/bash_completion.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#
-# /etc/profile.d/bash-completion
-#
-# check if we use bash
-if [ $SHELL = "/bin/bash" ]; then
-	bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
-	if [ -n "$PS1" ] && [ \( $bmajor -eq 2 -a $bminor '>' 04 \) -o $bmajor -ge 3 ] \
-	  && [ -f /etc/bash_completion ]; then # interactive shell
-	  # Source completion code
-	  . /etc/bash_completion
-	fi
-	unset bash bmajor bminor
+# Check for bash (and that we haven't already been sourced).
+[ -z "$BASH_VERSION" -o -n "$BASH_COMPLETION" ] && return
+
+# Check for interactive shell.
+if shopt -q login_shell; then
+    if [ -r /etc/bash_completion ]; then
+        # Source completion code.
+        . /etc/bash_completion
+    fi
 fi
+
