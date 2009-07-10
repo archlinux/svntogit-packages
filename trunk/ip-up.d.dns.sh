b23e9a42efcd10bb7eb886479e6a40fd2b86cb67
thomas <thomas@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2009-07-10 22:20:41 +0000

    upgpkg: ppp 2.4.4-9
    git-svn-id: file:///srv/repos/svn-packages/svn@45388 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- trunk/ip-up.d.dns.sh
+++ trunk/ip-up.d.dns.sh
@@ -3,4 +3,5 @@
 if [ "$USEPEERDNS" = "1" -a -f /etc/ppp/resolv.conf ]; then
   [ -e /etc/resolv.conf ] && mv /etc/resolv.conf /etc/resolv.conf.backup.${IFNAME}
   mv /etc/ppp/resolv.conf /etc/resolv.conf
+  chmod 644 /etc/resolv.conf
 fi
