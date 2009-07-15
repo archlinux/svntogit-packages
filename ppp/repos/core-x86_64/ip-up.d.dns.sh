5c88dc5de85587f1ac7adae8a46e3cd9b09165e2
thomas <thomas@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2009-07-15 11:10:14 +0000

    db-move: ppp removed by thomas for move to core
    git-svn-id: file:///srv/repos/svn-packages/svn@46293 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- ppp/repos/core-x86_64/ip-up.d.dns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ "$USEPEERDNS" = "1" -a -f /etc/ppp/resolv.conf ]; then
-  [ -e /etc/resolv.conf ] && mv /etc/resolv.conf /etc/resolv.conf.backup.${IFNAME}
-  mv /etc/ppp/resolv.conf /etc/resolv.conf
-fi
