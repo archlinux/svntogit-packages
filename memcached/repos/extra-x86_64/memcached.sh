44339e96e07a6e97bb07035df14c371cf09447f4
dan <dan@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2009-09-13 15:25:17 +0000

    Merged revisions 51908 via svnmerge from svn+ssh://svn.archlinux.org/srv/svn-packages/memcached/trunk
    ........
      r51908 | dan | 2009-09-13 10:25:00 -0500 (Sun, 13 Sep 2009) | 2 lines
      
      upgpkg: memcached 1.4.1-1
          bump to 1.4.x series, change conf a bit
    ........
    
    
    git-svn-id: file:///srv/repos/svn-packages/svn@51909 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- memcached/repos/extra-x86_64/memcached.sh
+++ memcached/repos/extra-x86_64/memcached.sh
@@ -4,16 +4,42 @@
 . /etc/rc.d/functions
 . /etc/conf.d/memcached
 
-PID=`pidof -o %PPID /usr/bin/memcached`
+PIDFILE='/var/run/memcached.pid'
+
+getpid() {
+  local pid
+  pid=$(cat $PIDFILE 2>/dev/null)
+  # if the process is no longer valid, don't return it
+  if [ -n "$pid" ]; then
+    if ! ps -p $pid >/dev/null; then
+      rm -f $PIDFILE
+      pid=""
+    fi
+  fi
+  echo $pid
+}
+
+PID="$(getpid)"
+
 case "$1" in
   start)
     stat_busy "Starting memcached"
-    [ -z "$PID" ] && /usr/bin/memcached -d $MEMCACHED_ARGS
-    if [ ! -z "$PID" -o $? -gt 0 ]; then
+    # memcached is retarded and doesn't write to the pidfile
+    # before it drops permissions
+    if [ -n "$PID" ]; then
+      stat_fail
+    elif [ -z "$MEMCACHED_USER"  ]; then
+      echo "MEMCACHED_USER must be defined in /etc/conf.d/memcached"
       stat_fail
     else
-      add_daemon memcached
-      stat_done
+      touch $PIDFILE && chown $MEMCACHED_USER $PIDFILE
+      /usr/bin/memcached -d -P $PIDFILE -u $MEMCACHED_USER $MEMCACHED_ARGS
+      if [ $? -gt 0 ]; then
+        stat_fail
+      else
+        add_daemon memcached
+        stat_done
+      fi
     fi
     ;;
   stop)
@@ -22,6 +48,7 @@ case "$1" in
     if [ $? -gt 0 ]; then
       stat_fail
     else
+      rm -f $PIDFILE
       rm_daemon memcached
       stat_done
     fi
