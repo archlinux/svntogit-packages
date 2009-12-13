495e4814763867fdb642f3282bef5cd1686cffd1
pierre <pierre@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2009-12-13 21:14:12 +0000

    Merged revisions 61235 via svnmerge from svn+ssh://gerolde.archlinux.org/srv/svn-packages/chromium/trunk
    ........
      r61235 | pierre | 2009-12-13 20:13:26 +0100 (So, 13 Dez 2009) | 1 line
      
       
    ........
    
    
    git-svn-id: file:///srv/repos/svn-packages/svn@61246 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- chromium/repos/testing-x86_64/chromium.sh
+++ chromium/repos/testing-x86_64/chromium.sh
@@ -2,4 +2,4 @@
 export PATH=$PATH:/usr/lib/chromium
 export CHROME_WRAPPER=/usr/lib/chromium/chromium
 export CHROME_DESKTOP=chromium.desktop
-/usr/lib/chromium/chromium $@
+exec /usr/lib/chromium/chromium $@
