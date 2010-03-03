2b8bd79483bb09c0d658c23c62b5c0556eb510b6
dgriffiths <dgriffiths@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2010-03-03 22:55:21 +0000

    archrelease: copy trunk to extra-x86_64
    git-svn-id: file:///srv/repos/svn-packages/svn@71090 eb2447ed-0c53-47e4-bac8-5bc4a241df78

---
+++ courier-imap/repos/extra-x86_64/courier-imap.rc.d
@@ -0,0 +1,55 @@
+CI_DAEMONS=
+[ -f /etc/conf.d/courier-imap ] && . /etc/conf.d/courier-imap
+[ -z $AUTO_AUTHDAEMON_LAG ]   && AUTO_AUTHDAEMON_LAG=2
+
+. /etc/rc.conf
+. /etc/rc.d/functions
+
+case "$1" in
+  start)
+    if [ $AUTO_AUTHDAEMON="true" ]; then
+      /etc/rc.d/authdaemond start
+      sleep ${AUTO_AUTHDAEMON_LAG}
+    fi
+    if [ ! -f /var/run/daemons/authdaemond ]; then
+      echo "ERROR: authdaemond is not running"
+      stat_fail
+      exit 1
+    fi
+    for daemon in $CI_DAEMONS; do
+      stat_busy "Starting Courier ${daemon}"
+      /usr/lib/courier-imap/${daemon}.rc start
+      if [ $? -gt 0 ]; then
+        stat_fail
+      else
+        add_daemon $daemon
+        stat_done
+      fi
+    done
+    if [ $AUTO_AUTHDAEMON="true" ]; then
+      /etc/rc.d/authdaemond stop
+    fi
+    ;;
+  stop)
+    for daemon in $CI_DAEMONS; do
+      stat_busy "Stopping Courier ${daemon}"
+      /usr/lib/courier-imap/$daemon.rc stop > /dev/null
+      if [ $? -gt 0 ]; then
+        stat_fail
+      else
+        rm_daemon $daemon
+        stat_done
+      fi
+    done
+    ;;
+  restart)
+    $0 stop
+    sleep 1
+    $0 start
+    ;;
+  *)
+    echo "usage: $0 {start|stop|restart}"  
+esac
+exit 0
+~                                                                                                                                                         
+~         
