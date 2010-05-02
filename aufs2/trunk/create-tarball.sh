e9d220c866a92d1c7dbf89fe868f225470b50260
pierre <pierre@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2010-05-02 10:11:05 +0000

    new upstream snapshot replace deprecated inotify with fsnotify cleanup install script
    git-svn-id: file:///srv/repos/svn-packages/svn@79236 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- aufs2/trunk/create-tarball.sh
+++ aufs2/trunk/create-tarball.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 AUFS2VERSION="-33"
 KERNELVERSION=2.6.33
-GITSNAPSHOT=20100225
+GITSNAPSHOT=20100425
 # aufs2 (no -xx) for the latest -rc version.
 git clone http://git.c3sl.ufpr.br/pub/scm/aufs/aufs2-standalone.git aufs2-standalone.git
 cd aufs2-standalone.git
