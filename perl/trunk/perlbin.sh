e6a22b0fbd9a412b516c326d5e053d530a1c8cde
kevin <kevin@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2010-05-10 00:55:05 +0000

    upgpkg: perl 5.12.0-1 Update to 5.12.0.  Fixed FS#13808, 17402, and 19136
    git-svn-id: file:///srv/repos/svn-packages/svn@79992 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- perl/trunk/perlbin.sh
+++ perl/trunk/perlbin.sh
@@ -1,2 +1,13 @@
 # Set path to perl scripts.
-export PATH="${PATH}:/usr/bin/perlbin/site:/usr/bin/perlbin/vendor:/usr/bin/perlbin/core"
+
+# Remove the perlbin dirs in the next release.
+scriptdirs="/usr/lib/perl5/site_perl/bin /usr/bin/perlbin/site
+/usr/lib/perl5/vendor_perl/bin /usr/bin/perlbin/vendor
+/usr/lib/perl5/core_perl/bin /usr/bin/perlbin/core"
+
+# Add dirs to path if they exist.
+for currdir in ${scriptdirs}; do
+	[ -d ${currdir} ] && perlpaths=${perlpaths}:${currdir}
+done
+export PATH=${PATH}:$perlpaths
+
