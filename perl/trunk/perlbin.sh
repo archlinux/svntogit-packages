4892388f216067062374e30db22d3c8573b86344
kevin <kevin@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2010-05-16 02:03:46 +0000

    upgpkg: in_bin - Fix FS#19411 and FS#19427
    git-svn-id: file:///srv/repos/svn-packages/svn@80366 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- perl/trunk/perlbin.sh
+++ perl/trunk/perlbin.sh
@@ -1,13 +1,19 @@
 # Set path to perl scripts.
-
 # Remove the perlbin dirs in the next release.
-scriptdirs="/usr/lib/perl5/site_perl/bin /usr/bin/perlbin/site
-/usr/lib/perl5/vendor_perl/bin /usr/bin/perlbin/vendor
-/usr/lib/perl5/core_perl/bin /usr/bin/perlbin/core"
 
 # Add dirs to path if they exist.
-for currdir in ${scriptdirs}; do
-	[ -d ${currdir} ] && perlpaths=${perlpaths}:${currdir}
-done
-export PATH=${PATH}:$perlpaths
+[ -d /usr/lib/perl5/site_perl/bin ] &&
+		PATH=$PATH:/usr/lib/perl5/site_perl/bin
+[ -d /usr/bin/perlbin/site ] &&
+		PATH=$PATH:/usr/bin/perlbin/site
+[ -d /usr/lib/perl5/vendor_perl/bin ] &&
+		PATH=$PATH:/usr/lib/perl5/vendor_perl/bin
+[ -d /usr/bin/perlbin/vendor ] &&
+		PATH=$PATH:/usr/bin/perlbin/vendor
+[ -d /usr/lib/perl5/core_perl/bin ] &&
+		PATH=$PATH:/usr/lib/perl5/core_perl/bin
+export PATH
+
+# If you have modules in non-standard directories you can add them here.
+#export PERLLIB=dir1:dir2
 
