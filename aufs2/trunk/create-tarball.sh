1d9146662baf5fbb3f60bad709bb7d14c22a9a16
tpowa <tpowa@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2010-05-17 07:59:57 +0000

    upgpkg: aufs2 2.6.34_20100517-1 rebuild  against .34 series
    git-svn-id: file:///srv/repos/svn-packages/svn@80414 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- aufs2/trunk/create-tarball.sh
+++ aufs2/trunk/create-tarball.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-AUFS2VERSION="-33"
-KERNELVERSION=2.6.33
-GITSNAPSHOT=20100425
+AUFS2VERSION="-34"
+KERNELVERSION=2.6.34
+GITSNAPSHOT=20100517
 # aufs2 (no -xx) for the latest -rc version.
 git clone http://git.c3sl.ufpr.br/pub/scm/aufs/aufs2-standalone.git aufs2-standalone.git
 cd aufs2-standalone.git
-git checkout origin/aufs2${AUFS2VERSION}
+#git checkout origin/aufs2${AUFS2VERSION}
+git checkout origin/aufs2
 #*** apply "aufs2-base.patch" and "aufs2-standalone.patch" to your kernel source files.
 cd ..
 rm -rf aufs2-${KERNELVERSIN}_${GITSNAPSHOT}
