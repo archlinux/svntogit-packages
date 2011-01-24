cf1b93d2ba644604892ef55a5639504ef13200af
tpowa <tpowa@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2011-01-24 19:08:46 +0000

    upgpkg: aufs2-util 20110124-1 rebuild against latest .37 snapshot
    git-svn-id: file:///srv/repos/svn-packages/svn@107424 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- aufs2-util/trunk/create-tarball.sh
+++ aufs2-util/trunk/create-tarball.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-GITSNAPSHOT=20100802
+GITSNAPSHOT=20110124
 # aufs2 (no -xx) for the latest -rc version.
 git clone http://git.c3sl.ufpr.br/pub/scm/aufs/aufs2-util.git aufs2-util.git
 cd aufs2-util.git
 #git checkout origin/aufs2${AUFS2VERSION}
-git checkout 
+git checkout origin/aufs2.1
 cd ..
 rm -rf aufs2-util-${GITSNAPSHOT}
 cp -a aufs2-util.git aufs2-util-${GITSNAPSHOT}
