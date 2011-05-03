78d46466ab87671af11ec90efb42fedafe3f9534
heftig <heftig@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2011-05-03 10:40:20 +0000

    upgpkg: libcanberra 0.28-1 Update to 0.28
    git-svn-id: file:///srv/repos/svn-packages/svn@122251 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- libcanberra/trunk/libcanberra-gtk-module.sh
+++ libcanberra/trunk/libcanberra-gtk-module.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-if [ -z "$GTK_MODULES" ] ; then
-	GTK_MODULES="canberra-gtk-module"
-else
-	GTK_MODULES="$GTK_MODULES:canberra-gtk-module"
-fi
-
-export GTK_MODULES
-
+case "$SESSION" in
+  GNOME)
+    # Done by gnome-settings-daemon
+    ;;
+  *)
+    if [ -z "$GTK_MODULES" ]; then
+      GTK_MODULES="canberra-gtk-module"
+    else
+      GTK_MODULES="$GTK_MODULES:canberra-gtk-module"
+    fi
+    export GTK_MODULES
+    ;;
+esac
