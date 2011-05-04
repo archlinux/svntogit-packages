a824ad0b83026f09c51e47bfdbfba2ef503d8bba
bisson <bisson@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2011-05-04 23:30:15 +0000

    archrelease: copy trunk to testing-i686
    git-svn-id: file:///srv/repos/svn-packages/svn@122610 eb2447ed-0c53-47e4-bac8-5bc4a241df78

---
+++ repos/testing-i686/pam.d
@@ -0,0 +1,7 @@
+account   required    pam_access.so
+account   required    pam_time.so
+account   required    pam_unix.so
+
+session   required    pam_limits.so
+session   required    pam_env.so
+session   required    pam_unix.so
