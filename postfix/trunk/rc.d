b22ce724677e8f357108393eebebd50457a3c04a
bisson <bisson@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2011-05-19 16:07:15 +0000

    fix FS#24340 with direct calls to /usr/sbin/postfix in rc.d
    git-svn-id: file:///srv/repos/svn-packages/svn@124325 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- postfix/trunk/rc.d
+++ postfix/trunk/rc.d
@@ -4,24 +4,23 @@
 . /etc/rc.d/functions
 
 name=postfix
-PID=$(pidof -o %PPID /usr/lib/postfix/master)
 
 case "$1" in
 start)
 	stat_busy "Starting $name daemon"
-	[[ -z "$PID" ]] && /usr/sbin/postfix start &>/dev/null \
+	/usr/sbin/postfix start &>/dev/null \
 	&& { add_daemon $name; stat_done; } \
 	|| { stat_fail; exit 1; }
 	;;
 stop)
 	stat_busy "Stopping $name daemon"
-	[[ -n "$PID" ]] && /usr/sbin/postfix stop &>/dev/null \
+	/usr/sbin/postfix stop &>/dev/null \
 	&& { rm_daemon $name; stat_done; } \
 	|| { stat_fail; exit 1; }
 	;;
 reload)
 	stat_busy "Reloading $name daemon"
-	[[ -n "$PID" ]] && kill -HUP $PID &>/dev/null \
+	/usr/sbin/postfix reload &>/dev/null \
 	&& { stat_done; } \
 	|| { stat_fail; exit 1; }
 	;;
