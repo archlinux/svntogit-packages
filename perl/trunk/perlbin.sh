063d7036e83f93a845d1a86468fd0f6af11d6d6c
angvp <angvp@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2011-06-23 21:11:34 +0000

    upgpkg: perl 5.14.1-1 Version bump + bug fixess
    git-svn-id: file:///srv/repos/svn-packages/svn@128400 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- perl/trunk/perlbin.sh
+++ perl/trunk/perlbin.sh
@@ -3,18 +3,13 @@
 # Added /usr/bin/*_perl dirs for scripts
 # Remove /usr/lib/perl5/*_perl/bin in next release
 
-[ -d /usr/bin/site_perl ] &&
-		PATH=$PATH:/usr/bin/site_perl
-[ -d /usr/lib/perl5/site_perl/bin ] &&
-		PATH=$PATH:/usr/lib/perl5/site_perl/bin
+[ -d /usr/bin/site_perl ] && PATH=$PATH:/usr/bin/site_perl
+[ -d /usr/lib/perl5/site_perl/bin ] && PATH=$PATH:/usr/lib/perl5/site_perl/bin
 
-[ -d /usr/bin/vendor_perl ] &&
-		PATH=$PATH:/usr/bin/vendor_perl
-[ -d /usr/lib/perl5/vendor_perl/bin ] &&
-		PATH=$PATH:/usr/lib/perl5/vendor_perl/bin
+[ -d /usr/bin/vendor_perl ] && PATH=$PATH:/usr/bin/vendor_perl
+[ -d /usr/lib/perl5/vendor_perl/bin ] && PATH=$PATH:/usr/lib/perl5/vendor_perl/bin
 
-[ -d /usr/bin/core_perl ] &&
-		PATH=$PATH:/usr/bin/core_perl
+[ -d /usr/bin/core_perl ] && PATH=$PATH:/usr/bin/core_perl
 
 export PATH
 
