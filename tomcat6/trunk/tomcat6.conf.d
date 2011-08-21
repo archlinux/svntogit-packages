c36ea67f4e511e8f560afe583dc961604982e3df
guillaume <guillaume@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2011-08-21 20:27:55 +0000

    upgpkg: tomcat6 6.0.33-2
    Fixed start at boot and pid number retrieval
    
    git-svn-id: file:///srv/repos/svn-packages/svn@136028 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- tomcat6/trunk/tomcat6.conf.d
+++ tomcat6/trunk/tomcat6.conf.d
@@ -1,3 +1,7 @@
+# The JAVA_HOME of the JVM for Tomcat to use
+# (compulsory to enable tomcat to start at boot)
+TOMCAT_JAVA_HOME=/usr/lib/jvm/java-6-openjdk
+
 # Tomcat home directory
 CATALINA_HOME=/usr/share/tomcat6
 
