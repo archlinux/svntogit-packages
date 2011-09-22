5ef3711cd045e973ffbc4b00e0720518045077f2
dreisner <dreisner@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2011-09-22 23:54:09 +0000

    Add varnish 3.0.1
    git-svn-id: file:///srv/repos/svn-packages/svn@138431 eb2447ed-0c53-47e4-bac8-5bc4a241df78

---
+++ varnish/trunk/varnish.conf.d
@@ -0,0 +1,9 @@
+#
+# Command line options to varnishd
+#
+
+VARNISHD_OPTS="-a 0.0.0.0:80 \
+               -b localhost:8080 \
+               -T localhost:6082 \
+               -s malloc,64M
+               -u nobody -g nobody"
