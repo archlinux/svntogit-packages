0edf1cfe90d2dfe62f3f72770d4babb87638f2e4
bisson <bisson@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2012-03-29 15:43:18 +0000

    db-move: moved clamav from [testing] to [extra] (i686)
    git-svn-id: file:///srv/repos/svn-packages/svn@154495 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- repos/extra-i686/rc.d
+++ repos/extra-i686/rc.d
@@ -3,8 +3,7 @@
 . /etc/rc.conf
 . /etc/rc.d/functions
 
-[[ -f /etc/conf.d/clamav ]] && . /etc/conf.d/clamav
-[[ -d /var/run/clamav ]] || install -d -o clamav -g root /var/run/clamav
+[[ -f /etc/conf.d/clamav ]] && source /etc/conf.d/clamav
 
 PID_FC=$(pidof -o %PPID /usr/bin/freshclam)
 PID_CD=$(pidof -o %PPID /usr/sbin/clamd)
@@ -24,7 +23,7 @@ case "$1" in
 		sleep 1
 		if [[ $START_FRESHCLAM = yes ]]; then
 			stat_busy "Starting FreshClam"
-			[[ -z $PID_FC ]] && /usr/bin/freshclam -p /var/run/clamav/freshclam.pid -d $FRESHCLAM_OPTS
+			[[ -z $PID_FC ]] && /usr/bin/freshclam -p /run/clamav/freshclam.pid -d $FRESHCLAM_OPTS
 			if [[ $? -gt 0 ]]; then
 					stat_fail
 			else
