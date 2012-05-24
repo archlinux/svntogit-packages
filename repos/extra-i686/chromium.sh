cc9592b0047a649ba6191a94c5ef88f2056465d3
foutrelis <foutrelis@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2012-05-24 07:56:26 +0000

    archrelease: copy trunk to extra-i686, extra-x86_64
    git-svn-id: file:///srv/repos/svn-packages/svn@159426 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- repos/extra-i686/chromium.sh
+++ repos/extra-i686/chromium.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
-export CHROME_WRAPPER=/usr/lib/chromium/chromium
+#!/bin/bash
+
+# Allow users to override command-line options
+# Based on Gentoo's chromium package (and by extension, Debian's)
+if [[ -f /etc/chromium/default ]]; then
+	. /etc/chromium/default
+fi
+
+# Prefer user defined CHROMIUM_USER_FLAGS (from env) over system
+# default CHROMIUM_FLAGS (from /etc/chromium/default)
+CHROMIUM_FLAGS=${CHROMIUM_USER_FLAGS:-$CHROMIUM_FLAGS}
+
+export CHROME_WRAPPER=$(readlink -f "$0")
 export CHROME_DESKTOP=chromium.desktop
-exec /usr/lib/chromium/chromium "$@"
+
+exec /usr/lib/chromium/chromium $CHROMIUM_FLAGS "$@"
