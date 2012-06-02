249e797f8c43a61d525cb70c54f612d87bc879ea
andyrtr <andyrtr@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2012-06-02 21:42:39 +0000

    upgpkg: libx11 1.5.0-1
    upstream update 1.5.0; fix environment variable /usr/share/:/usr/local/share/:/usr/share order FS#30113
    
    git-svn-id: file:///srv/repos/svn-packages/svn@160632 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- libx11/trunk/xorg.sh
+++ libx11/trunk/xorg.sh
@@ -3,9 +3,9 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 
 if [ -z $XDG_DATA_DIRS ]; then
-	export XDG_DATA_DIRS=/usr/share/:/usr/local/share/
+	export XDG_DATA_DIRS=/usr/local/share/:/usr/share/
 else
-	export XDG_DATA_DIRS=/usr/share/:/usr/local/share/:$XDG_DATA_DIRS
+	export XDG_DATA_DIRS=/usr/local/share/:/usr/share/:$XDG_DATA_DIRS
 fi
 
 if [ -z $XDG_CONFIG_DIRS ]; then
