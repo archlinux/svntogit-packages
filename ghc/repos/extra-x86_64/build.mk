87913a40860419dd4bb24d85ff82b154c6254fce
tdziedzic <tdziedzic@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2012-06-13 13:44:00 +0000

    db-move: moved ghc from [testing] to [extra] ( i686,  x86_64)
    git-svn-id: file:///srv/repos/svn-packages/svn@161653 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- ghc/repos/extra-x86_64/build.mk
+++ ghc/repos/extra-x86_64/build.mk
@@ -117,7 +117,7 @@ HADDOCK_DOCS       = NO
 BUILD_DOCBOOK_HTML = NO
 BUILD_DOCBOOK_PS   = NO
 BUILD_DOCBOOK_PDF  = NO
-LAX_DEPENDENCIES   = NO
+LAX_DEPENDENCIES   = YES
 
 endif
 
@@ -134,7 +134,7 @@ HADDOCK_DOCS       = NO
 BUILD_DOCBOOK_HTML = NO
 BUILD_DOCBOOK_PS   = NO
 BUILD_DOCBOOK_PDF  = NO
-LAX_DEPENDENCIES   = NO
+LAX_DEPENDENCIES   = YES
 
 # After stage 1 and the libraries have been built, you can uncomment this line:
 
