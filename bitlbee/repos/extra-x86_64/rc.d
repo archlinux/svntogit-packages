6e138b875bf32e088caccb308c0b00382b327553
allan <allan@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2012-07-14 13:10:00 +0000

    db-move: moved bitlbee from [testing] to [extra] ( i686,  x86_64)
    git-svn-id: file:///srv/repos/svn-packages/svn@163519 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- bitlbee/repos/extra-x86_64/rc.d
+++ bitlbee/repos/extra-x86_64/rc.d
@@ -3,38 +3,47 @@
 . /etc/rc.conf
 . /etc/rc.d/functions
 
-name=bitlbee
-PID=$(pidof -o %PPID /usr/sbin/bitlbee)
-install -o bitlbee -g bitlbee -d -m 755 /var/run/bitlbee
+pidfile=/run/bitlbee/bitlbee.pid
+if [[ -r $pidfile ]]; then
+  read -r PID < "$pidfile"
+  if [[ ! -d /proc/$PID ]]; then
+    # stale pidfile
+    unset PID
+    rm -f "$pidfile"
+  fi
+fi
 
-case "$1" in
-start)
-	stat_busy "Starting $name daemon"
-	[[ -z "$PID" ]] && {
-		rm -f /var/run/bitlbee/bitlbee.pid
-		su -s /bin/sh -c '/usr/sbin/bitlbee -F' bitlbee
-	} && {
-		add_daemon $name
-		stat_done
-	} || { stat_fail; exit 1; }
-	;;
-stop)
-	stat_busy "Stopping $name daemon"
-	[[ -n "$PID" ]] && {
-		kill $PID &>/dev/null
-	} && {
-		rm -f /var/run/bitlbee/bitlbee.pid
-		rm_daemon $name
-		stat_done
-	} || { stat_fail; exit 1; }
-	;;
-restart)
-	$0 stop
-	sleep 3
-	$0 start
-	;;
-*)
-	echo "usage: $0 {start|stop|restart}"
-	exit 1
-	;;
+case $1 in
+  start)
+    stat_busy "Starting bitlbee daemon"
+    if [[ $PID ]] || ! su -s /bin/sh -c '/usr/sbin/bitlbee -F' bitlbee; then
+      stat_fail
+      exit 1
+    else
+      add_daemon bitlbee
+      stat_done
+    fi
+    ;;
+  stop)
+    stat_busy "Stopping bitlbee daemon"
+    if [[ -z $PID ]] || ! kill $PID &>/dev/null; then
+      stat_fail
+      exit 1
+    else
+      rm -f "$pidfile"
+      rm_daemon bitlbee
+      stat_done
+    fi
+    ;;
+  restart)
+    $0 stop
+    sleep 3
+    $0 start
+    ;;
+  *)
+    echo "usage: $0 {start|stop|restart}"
+    exit 1
+    ;;
 esac
+
+# vim: set ft=sh et ts=2 sw=2:
