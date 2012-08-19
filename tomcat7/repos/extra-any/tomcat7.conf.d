3c0382b7ecb2f654b1856c9051fab9080614294d
guillaume <guillaume@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2012-08-19 14:16:33 +0000

    archrelease: copy trunk to extra-any
    git-svn-id: file:///srv/repos/svn-packages/svn@165438 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- tomcat7/repos/extra-any/tomcat7.conf.d
+++ tomcat7/repos/extra-any/tomcat7.conf.d
@@ -1,3 +1,6 @@
+# Configuration file for Tomcat 7
+# Have a look at /usr/share/tomcat7/bin/catalina.sh for a complete list of environment variables
+
 # The JAVA_HOME of the JVM for Tomcat to use
 # (compulsory to enable tomcat to start at boot)
 TOMCAT_JAVA_HOME=/usr/lib/jvm/java-7-openjdk
@@ -9,7 +12,7 @@ CATALINA_HOME=/usr/share/tomcat7
 # binary distribution shared among multiple users on the same server.  To make
 # this possible, you can set the $CATALINA_BASE environment variable to the
 # directory that contains the files for your 'personal' Tomcat instance.
-CATALINA_BASE=${CATALINA_HOME}
+CATALINA_BASE=/usr/share/tomcat7
 
 # Tomcat additional commandline options
 CATALINA_OPTS=
