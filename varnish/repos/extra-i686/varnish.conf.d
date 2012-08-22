406c4be494c1be1810c78cd437c3292f26016e06
dreisner <dreisner@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2012-08-22 17:11:02 +0000

    archrelease: copy trunk to extra-i686, extra-x86_64
    git-svn-id: file:///srv/repos/svn-packages/svn@165520 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- varnish/repos/extra-i686/varnish.conf.d
+++ varnish/repos/extra-i686/varnish.conf.d
@@ -2,10 +2,10 @@
 # Command line options to varnishd
 #
 
+VARNISH_CFG="/etc/varnish/default.vcl"
+
 VARNISHD_OPTS="-a 0.0.0.0:80 \
-               -b localhost:8080 \
+               -f $VARNISH_CFG \
                -T localhost:6082 \
                -s malloc,64M
                -u nobody -g nobody"
-
-VARNISH_CFG="/etc/varnish/default.vcl"
