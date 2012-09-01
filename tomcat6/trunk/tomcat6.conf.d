1d15471cd706cf4dbbd5a1bdbc84ac0ce0170b5d
guillaume <guillaume@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2012-09-01 17:27:38 +0000

    Added systemd service file support
    git-svn-id: file:///srv/repos/svn-packages/svn@165855 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- tomcat6/trunk/tomcat6.conf.d
+++ tomcat6/trunk/tomcat6.conf.d
@@ -1,3 +1,7 @@
+# Configuration file for Tomcat 6 when started with 'initscripts'
+# This file is NOT used when tomcat is started with systemd
+# Have a look at /usr/share/tomcat6/bin/catalina.sh for a complete list of environment variables
+
 # The JAVA_HOME of the JVM for Tomcat to use
 # (compulsory to enable tomcat to start at boot)
 TOMCAT_JAVA_HOME=/usr/lib/jvm/java-7-openjdk
@@ -9,7 +13,7 @@ CATALINA_HOME=/usr/share/tomcat6
 # binary distribution shared among multiple users on the same server.  To make
 # this possible, you can set the $CATALINA_BASE environment variable to the
 # directory that contains the files for your 'personal' Tomcat instance.
-CATALINA_BASE=${CATALINA_HOME}
+CATALINA_BASE=/usr/share/tomcat6
 
 # Tomcat additional commandline options
 CATALINA_OPTS=
