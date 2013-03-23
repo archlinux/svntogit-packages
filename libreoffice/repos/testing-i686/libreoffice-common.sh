7aad400cc7c0aa9bff96cf7044a1546d07e1a94b
bpiotrowski <bpiotrowski@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2013-03-23 07:24:00 +0000

    db-move: moved libreoffice from [testing] to [extra] (i686, x86_64)
    git-svn-id: file:///srv/repos/svn-packages/svn@180554 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- libreoffice/repos/testing-i686/libreoffice-common.sh
+++
@@ -1,8 +0,0 @@
-#export PYTHONPATH="$PYTHONPATH:/usr/lib/libreoffice/program"
-
-# to force a certain look'n feel
-
-#export SAL_USE_VCLPLUGIN=gen
-#export SAL_USE_VCLPLUGIN=kde4
-#export SAL_USE_VCLPLUGIN=gtk
-#export SAL_USE_VCLPLUGIN=gtk3
