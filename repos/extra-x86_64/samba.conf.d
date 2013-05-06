bb4842fa54454ea6ea26a3a938d66f7445199423
tpowa <tpowa@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2013-05-06 12:15:54 +0000

    archrelease: copy trunk to extra-i686, extra-x86_64
    git-svn-id: file:///srv/repos/svn-packages/svn@184349 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- repos/extra-x86_64/samba.conf.d
+++ repos/extra-x86_64/samba.conf.d
@@ -1,7 +1,18 @@
-#
-# Configuration for the samba init script
-#
-
-# space separated list of daemons to launch
-SAMBA_DAEMONS=(smbd nmbd)
-#SAMBA_DAEMONS=(smbd nmbd winbindd)
+## Path:           Network/Samba
+## Description:    Samba process options
+## Type:           string
+## Default:        ""
+## ServiceRestart: samba
+SAMBAOPTIONS=""
+## Type:           string
+## Default:        ""
+## ServiceRestart: smb
+SMBDOPTIONS=""
+## Type:           string
+## Default:        ""
+## ServiceRestart: nmb
+NMBDOPTIONS=""
+## Type:           string
+## Default:        ""
+## ServiceRestart: winbind
+WINBINDOPTIONS=""
