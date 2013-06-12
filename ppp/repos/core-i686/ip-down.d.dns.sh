d8ab875b03f03d0213b674731e63d6714cd1a327
thomas <thomas@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2013-06-12 15:53:29 +0000

    db-move: moved ppp from [testing] to [core] (i686, x86_64)
    git-svn-id: file:///srv/repos/svn-packages/svn@188198 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- ppp/repos/core-i686/ip-down.d.dns.sh
+++ ppp/repos/core-i686/ip-down.d.dns.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-[ -e /etc/resolv.conf.backup.${IFNAME} ] && mv /etc/resolv.conf.backup.${IFNAME} /etc/resolv.conf
+if [ -x /usr/bin/resolvconf ]; then
+  /usr/bin/resolvconf -fd ${IFNAME}
+else
+  [ -e /etc/resolv.conf.backup.${IFNAME} ] && mv /etc/resolv.conf.backup.${IFNAME} /etc/resolv.conf
+fi
