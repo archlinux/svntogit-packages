bcff144cb1632a41eb4df6f651fdb75e8de17723
heftig <heftig@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2014-03-04 15:33:23 +0000

    archrelease: copy trunk to extra-i686, extra-x86_64
    git-svn-id: file:///srv/repos/svn-packages/svn@206668 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- eclipse/repos/extra-i686/eclipse.sh
+++ eclipse/repos/extra-i686/eclipse.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export ECLIPSE_HOME=/usr/share/eclipse
+export ECLIPSE_HOME=/usr/lib/eclipse
 export GDK_NATIVE_WINDOWS=true
-${ECLIPSE_HOME}/eclipse "$@"
+exec ${ECLIPSE_HOME}/eclipse "$@"
