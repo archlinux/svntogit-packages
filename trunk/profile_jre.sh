45a9c77542a30027ed181ada97e54b662916e35c
guillaume <guillaume@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2014-08-02 18:58:05 +0000

    Rewrite archlinux-java for cleanup
    git-svn-id: file:///srv/repos/svn-packages/svn@218519 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- trunk/profile_jre.sh
+++ trunk/profile_jre.sh
@@ -1,4 +1,3 @@
 # Do not change this unless you want to completely by-pass Arch Linux' way
 # of handling Java versions and vendors. Instead, please use script `archlinux-java`
-# to change what /usr/lib/jvm/java-default-runtime points at.
 export JAVA_HOME=/usr/lib/jvm/java-default-runtime
