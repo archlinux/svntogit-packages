477283c9a48f60c894d1f8a2cc451cdb62b34c0d
guillaume <guillaume@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2014-08-19 10:35:12 +0000

    archrelease: copy trunk to extra-any
    git-svn-id: file:///srv/repos/svn-packages/svn@220243 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- repos/extra-any/install_java-common.sh
+++ repos/extra-any/install_java-common.sh
@@ -1,7 +1,7 @@
 post_install() {
   echo 'Installing multiple non-conflicting Java environments is now supported'
   echo 'Check the following wiki section for more info:'
-  echo '  https://wiki.archlinux.org/index.php/Java#Multiple_official_Java_environments'
+  echo '  https://wiki.archlinux.org/index.php/Java'
   echo 'Please make sure JAVA_HOME is set to /usr/lib/jvm/java-default-runtime'
   echo 'If not, you may have to logout and login again to set it according to /etc/profile.d/jre.sh'
 }
