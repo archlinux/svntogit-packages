e1fe24234d1ff8d8fc5b7e4e3ca3ff023e517bad
guillaume <guillaume@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2014-10-11 15:38:25 +0000

    FS#41883 FS#41901 add bin links to package. Be quieter.
    git-svn-id: file:///srv/repos/svn-packages/svn@224209 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- java-common/trunk/install_java-common.sh
+++
@@ -1,8 +0,0 @@
-post_install() {
-  echo 'Please make sure JAVA_HOME is set to /usr/lib/jvm/default'
-  echo 'If not, you may have to logout and login again to set it according to /etc/profile.d/jre.sh'
-}
-
-post_upgrade() {
-  post_install "$@"
-}
--- java-common/trunk/profile_jre.sh
+++ java-common/trunk/profile_jre.sh
@@ -1,6 +1,6 @@
 # Do not change this unless you want to completely by-pass Arch Linux' way
 # of handling Java versions and vendors. Instead, please use script `archlinux-java`
-export JAVA_HOME=/usr/lib/jvm/default
+export PATH=${PATH}:/usr/lib/jvm/default/bin
 
 # Enable this for non-reparenting window managers
 #export _JAVA_AWT_WM_NONREPARENTING=1
