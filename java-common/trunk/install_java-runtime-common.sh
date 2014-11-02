8073e261ca30ba9d1c80c543118cee65bec6d63b
guillaume <guillaume@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2014-11-02 13:38:41 +0000

    Remove _JAVA_AWT_WM_NONREPARENTING from jre.sh
    git-svn-id: file:///srv/repos/svn-packages/svn@225550 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- java-common/trunk/install_java-runtime-common.sh
+++ java-common/trunk/install_java-runtime-common.sh
@@ -1,3 +1,10 @@
+pre_upgrade() {
+  if grep -q '^export _JAVA_AWT_WM_NONREPARENTING=1' /etc/profile.d/jre.sh > /dev/null 2>&1; then
+    echo "Variable '_JAVA_AWT_WM_NONREPARENTING' should not be set in /etc/profile.d/jre.sh any more"
+    echo "Please declare it in ~/.xinitrc or such"
+  fi
+}
+
 post_install() {
   echo 'For the complete set of Java binaries to be available in your PATH,'
   echo 'you need to re-login or source /etc/profile.d/jre.sh'
