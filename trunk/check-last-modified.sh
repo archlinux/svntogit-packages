cd36d70a4464e3c13c13a0447a386f05bb1f65bc
foutrelis <foutrelis@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2015-02-15 11:40:57 +0000

    upgpkg: geoip-database 20150203-1
    New upstream release.
    
    git-svn-id: file:///srv/repos/svn-packages/svn@231449 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- trunk/check-last-modified.sh
+++ trunk/check-last-modified.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -e
 
+# Unicode characters taken from pactree.c
+UNICODE_IS_FUN="\u2514\u2500"
+
+verbose=0
+if [[ $1 == -v ]]; then
+	verbose=1
+fi
+
 URLS=(https://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
       https://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz)
 
 for url in ${URLS[@]}; do
 	last_mod=$(curl -sI $url | grep ^Last-Modified: | cut -d' ' -f2- | tr -d '\r')
 	echo "$last_mod (${url##*/})"
+
+	if ((verbose)); then
+		build_id=$(curl -s $url | gzip -cd | grep -ao '[0-9]* Build [0-9]*')
+		echo -e "${UNICODE_IS_FUN}$build_id"
+	fi
 done
