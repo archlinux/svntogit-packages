b61d0502f848e97ddb9412c145fad6d00c199a64
foutrelis <foutrelis@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2016-03-25 13:04:03 +0000

    upgpkg: geoip-database 20160301-2
    Ship city and ASN databases in a new split package (FS#48391).
    
    git-svn-id: file:///srv/repos/svn-packages/svn@262532 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- trunk/check-last-modified.sh
+++ trunk/check-last-modified.sh
@@ -9,14 +9,18 @@ if [[ $1 == -v ]]; then
 fi
 
 URLS=(https://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
-      https://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz)
+      https://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz
+      https://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+      https://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz
+      https://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
+	  https://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz)
 
 for url in ${URLS[@]}; do
 	last_mod=$(curl -sI $url | grep ^Last-Modified: | cut -d' ' -f2- | tr -d '\r')
 	echo "$last_mod (${url##*/})"
 
 	if ((verbose)); then
-		build_id=$(curl -s $url | gzip -cd | grep -ao '[0-9]* Build [0-9]*')
+		build_id=$(curl -s $url | gzip -cd | grep -aoE '[0-9]{8} Build [0-9]*')
 		echo -e "${UNICODE_IS_FUN}$build_id"
 	fi
 done
