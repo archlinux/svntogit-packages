6f193dc016a09ed1bc03af546980e960886df4fe
foutrelis <foutrelis@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2017-05-10 21:30:13 +0000

    check-last-modified.sh: Fix for lowercase response headers
    git-svn-id: file:///srv/repos/svn-packages/svn@295572 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- geoip-database/trunk/check-last-modified.sh
+++ geoip-database/trunk/check-last-modified.sh
@@ -13,10 +13,10 @@ URLS=(https://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.d
       https://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
       https://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz
       https://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
-	  https://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz)
+      https://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz)
 
 for url in ${URLS[@]}; do
-	last_mod=$(curl -sI $url | grep ^Last-Modified: | cut -d' ' -f2- | tr -d '\r')
+	last_mod=$(curl -sI $url | grep -i ^Last-Modified: | cut -d' ' -f2- | tr -d '\r')
 	echo "$last_mod (${url##*/})"
 
 	if ((verbose)); then
