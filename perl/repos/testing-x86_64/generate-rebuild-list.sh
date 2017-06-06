11bf495baa5a28e73c4637b221324a4901b296d8
bluewind <bluewind@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2017-06-06 09:15:23 +0000

    db-move: moved perl from [testing] to [core] (i686, x86_64)
    git-svn-id: file:///srv/repos/svn-packages/svn@297856 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- perl/repos/testing-x86_64/generate-rebuild-list.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pkgfile -r "^/usr/lib/perl5/vendor_perl/auto/.*\.so$" | sed 's#^.*/##' | sort -u
-ssh soyuz.archlinux.org PATH=/usr/local/bin:\$PATH\; /home/bluewind/bin/sogrep-all libperl.so
