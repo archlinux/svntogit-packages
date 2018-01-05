27623aac5465d29641a6e32da05e3340c097df89
seblu <seblu@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2018-01-05 01:48:02 +0000

    upgpkg: filesystem 2018.1-1
    - Remove LC_ALL
    - Restore previous loading of /etc/locale.sh
    
    
    git-svn-id: file:///srv/repos/svn-packages/svn@314052 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- filesystem/trunk/locale.sh
+++ filesystem/trunk/locale.sh
@@ -2,11 +2,14 @@
 
 # load locale.conf in XDG paths.
 # /etc/locale.conf loads and overrides by kernel command line is done by systemd
+# But we override it here, see FS#56688
 if [ -z "$LANG" ]; then
   if [ -n "$XDG_CONFIG_HOME" ] && [ -r "$XDG_CONFIG_HOME/locale.conf" ]; then
     . "$XDG_CONFIG_HOME/locale.conf"
   elif [ -n "$HOME" ] && [ -r "$HOME/.config/locale.conf" ]; then
     . "$HOME/.config/locale.conf"
+  elif [ -r /etc/locale.conf ]; then
+    . /etc/locale.conf
   fi
 fi
 
@@ -14,6 +17,5 @@ fi
 LANG=${LANG:-C}
 
 # export all locale (7) variables when they exist
-export LANG LANGUAGE LC_ALL LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY \
- LC_MESSAGES LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_IDENTIFICATION \
- LC_MEASUREMENT
+export LANG LANGUAGE LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_NAME\
+ LC_MESSAGES LC_NAME LC_ADDRESS LC_TELEPHONE LC_IDENTIFICATION LC_MEASUREMENT
