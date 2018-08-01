1d532a7bbb317e916dfe912413944446e3e8921e
bluewind <bluewind@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2018-08-01 08:52:50 +0000

    upgpkg: perl 5.28.0-1
    upstream update
    
    git-svn-id: file:///srv/repos/svn-packages/svn@329919 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- perl/trunk/perlbin.fish
+++ perl/trunk/perlbin.fish
@@ -3,7 +3,7 @@
 
 if status --is-login
     for perldir in /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
-        if test -d $perldir
+        if test -d $perldir; and not contains $perldir $PATH
             set PATH $PATH $perldir
         end
     end
