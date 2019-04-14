bf3de2c1321a40fbb3491d8529b8b915b0b36679
bpiotrowski <bpiotrowski@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2019-04-14 18:07:30 +0000

    db-move: moved flatpak from [testing] to [extra] (x86_64)
    git-svn-id: file:///srv/repos/svn-packages/svn@350783 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- flatpak/repos/testing-x86_64/flatpak-bindir.sh
+++
@@ -1,7 +0,0 @@
-if [ -d "$HOME" ] && [ -d "$HOME/.local/share/flatpak/exports/bin" ]; then
-  PATH="$PATH:$HOME/.local/share/flatpak/exports/bin"
-fi
-
-if [ -d /var/lib/flatpak/exports/bin ]; then
-  PATH="$PATH:/var/lib/flatpak/exports/bin"
-fi
