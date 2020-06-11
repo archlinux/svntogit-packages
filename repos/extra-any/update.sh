08826a369a0bfb0ab8901dbc013c1fdb1f8806f4
foutrelis <foutrelis@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2020-06-11 22:11:27 +0000

    archrelease: copy trunk to extra-any
    git-svn-id: file:///srv/repos/svn-packages/svn@389040 eb2447ed-0c53-47e4-bac8-5bc4a241df78

---
+++ repos/extra-any/update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eo pipefail
+
+readonly upstream=https://mailfud.org/geoip-legacy/
+
+version=$(curl -s "$upstream" | grep -Po 'CSV_\K[0-9]{8}' | sort -u)
+
+if [[ ${#version} != 8 ]]; then
+	echo >&2 'Unable to determine upstream database version'
+	exit 1
+fi
+
+pkgver=$(. PKGBUILD; echo $pkgver)
+
+if ((pkgver >= version)); then
+	echo >&2 "Already at version $pkgver (>= $version upstream)"
+	exit 1
+fi
+
+ssh -T sources.archlinux.org <<-EOF
+	mkdir -p /srv/sources/other/packages/geoip-database/$version &&
+	cd       /srv/sources/other/packages/geoip-database/$version &&
+	wget -nc $upstream/GeoIP{,City,ASNum}{,v6}.dat.gz
+EOF
+
+sed -i -e "/^pkgver=/c pkgver=$version" -e "/^pkgrel=/c pkgrel=1" PKGBUILD
