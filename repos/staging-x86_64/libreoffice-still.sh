9b737aa93d08f803091d20cc78d44447c610f629
felixonmars <felixonmars@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2020-12-19 11:30:32 +0000

    archrelease: copy trunk to staging-x86_64
    git-svn-id: file:///srv/repos/svn-packages/svn@404618 eb2447ed-0c53-47e4-bac8-5bc4a241df78

---
+++ repos/staging-x86_64/libreoffice-still.sh
@@ -0,0 +1,7 @@
+# to force a certain look'n feel
+
+#export SAL_USE_VCLPLUGIN=gen
+#export SAL_USE_VCLPLUGIN=gtk3_kde5
+#export SAL_USE_VCLPLUGIN=kf5
+#export SAL_USE_VCLPLUGIN=qt5
+#export SAL_USE_VCLPLUGIN=gtk3
