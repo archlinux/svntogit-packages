b97ca4af16cdf7c7213dceb9d7bcfcb6f55a1935
heftig <heftig@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2021-06-03 12:15:28 +0000

    1.11.1-2: Use append_path in bindir script and check XDG_DATA_HOME
    git-svn-id: file:///srv/repos/svn-packages/svn@417045 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- flatpak/trunk/flatpak-bindir.sh
+++ flatpak/trunk/flatpak-bindir.sh
@@ -1,7 +1,9 @@
-if [ -d "$HOME" ] && [ -d "$HOME/.local/share/flatpak/exports/bin" ]; then
-  PATH="$PATH:$HOME/.local/share/flatpak/exports/bin"
+if [ -n "$XDG_DATA_HOME" ] && [ -d "$XDG_DATA_HOME/flatpak/exports/bin" ]; then
+  append_path "$XDG_DATA_HOME/flatpak/exports/bin"
+elif [ -n "$HOME" ] && [ -d "$HOME/.local/share/flatpak/exports/bin" ]; then
+  append_path "$HOME/.local/share/flatpak/exports/bin"
 fi
 
 if [ -d /var/lib/flatpak/exports/bin ]; then
-  PATH="$PATH:/var/lib/flatpak/exports/bin"
+  append_path /var/lib/flatpak/exports/bin
 fi
