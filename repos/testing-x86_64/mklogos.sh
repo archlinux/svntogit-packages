10e701c1358990589c22713b55d2ee16c29c2858
heftig <heftig@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2021-11-11 09:16:36 +0000

    archrelease: copy trunk to testing-x86_64
    git-svn-id: file:///srv/repos/svn-packages/svn@427884 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- repos/testing-x86_64/mklogos.sh
+++ repos/testing-x86_64/mklogos.sh
@@ -9,13 +9,11 @@ mklogo() (
 
   rm -f -- "$remote" "$name.svg.tmp" "$name.svg" "$name.png"
   wget -O "$remote" -- "$baseurl/$remote"
-  rsvg-convert -f svg -h 256 -o "$name.svg.tmp" -- "$remote"
+  rsvg-convert -f svg -w 256 -o "$name.svg.tmp" -- "$remote"
   scour --strip-xml-prolog --remove-descriptive-elements \
       --enable-comment-stripping --enable-viewboxing \
       --enable-id-stripping --shorten-ids \
       -i "$name.svg.tmp" -o "$name.svg"
-  rsvg-convert -f png -z 1 -o "$name.png" -- "$name.svg"
-  optipng -strip all -o4 -fix -- "$name.png"
   rm -- "$remote" "$name.svg.tmp"
 )
 
@@ -24,3 +22,6 @@ mklogo archlinux-logo-text      archlinux-logo-dark-scalable.svg  &
 mklogo archlinux-logo-text-dark archlinux-logo-light-scalable.svg &
 
 wait
+
+rsvg-convert -f png -z 1 -o archlinux-logo.png archlinux-logo.svg
+optipng -strip all -o4 -fix -- archlinux-logo.png
