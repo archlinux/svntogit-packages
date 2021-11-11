ec22a284685ae4284b91c58f6175bc39ef3ea1ac
seblu <seblu@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2021-11-11 03:06:48 +0000

    dkms 3.0.0
    - rewrite hooh.sh to fix FS#72135
    - move alpm script in a new location
    - add missing dkms script dependencies
    - fix lib path in sign_helper.sh
    - drop debian scipts
    - closes FS#72343
    - closes FS#72342
    
    
    
    git-svn-id: file:///srv/repos/svn-packages/svn@427877 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- dkms/trunk/hook.sh
+++ dkms/trunk/hook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright © 2018-2020, Sébastien Luttringer
+# Copyright © 2018-2021, Sébastien Luttringer
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -22,7 +22,7 @@ run() {
   echo "==> $*"
   "$@" > /dev/null
   local ret=$?
-  (( $ret )) && echo "==> Warning, \`$*' returned $ret"
+  (( $ret )) && echo "==> WARNING: \`$*' exited $ret"
   return $ret
 }
 
@@ -52,127 +52,166 @@ check_buildexclusive() {
   [[ "$3" =~ $BUILD_EXCLUSIVE_KERNEL ]]
 }
 
-# handle actions on module addition/upgrade/removal
-# $1: module name
-# $2: module version
-parse_module() {
+# list all kernel versions
+all_kver() {
   pushd "$install_tree" >/dev/null
   local path
   for path in */build/; do
-    local kver="${path%%/*}"
-    dkms_register "$1" "$2" "$kver"
+    echo "${path%%/*}"
   done
   popd >/dev/null
 }
 
-# handle actions on kernel addition/upgrade/removal
+# list all module name/version for a specific kernel version
 # $1: kernel version
-parse_kernel() {
+all_nv_from_kver() {
   local path
   for path in "$source_tree"/*-*/dkms.conf; do
     if [[ -f "$path" && "$path" =~ ^$source_tree/([^/]+)-([^/]+)/dkms\.conf$ ]]; then
-      dkms_register "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}" "$1"
+      echo "${BASH_REMATCH[1]}/${BASH_REMATCH[2]}"
     fi
   done
 }
 
-# register a dkms module to install/remove
-# this function suppress echo call for a module
-# $1: module name, $2: module version, $3: kernel version
-dkms_register() {
-  DKMS_MODULES["$1/$2/$3"]=''
+# list of modules/version for installed/built kernel version
+# $1: kernel version
+built_nv_from_kver() {
+  local line
+  dkms status -k "$1" | while read -r line; do
+    if [[ "$line" =~ ^([^/]+/[^,]+)", $1, "[^:]+": "(built|installed) ]]; then
+      echo "${BASH_REMATCH[1]}"
+    fi
+  done
+}
+
+# list installed or built kernel version for a specific module version
+# $1 : module name/module version
+built_kver_from_nv() {
+  local line
+  dkms status "$1" | while read -r line; do
+    if [[ "$line" =~ ^"$1, "([^,]+)", "[^:]+": "(built|installed) ]]; then
+      echo "${BASH_REMATCH[1]}"
+    fi
+  done
 }
 
 # install registered modules
 dkms_install() {
-  local nvk mod mver kver
-  local -i retry=1
-  local -A dmods=()
+  # list of modules to build for a specific kernel
+  local -A tobuild=()
 
+  # add new/updated modules for all kernels to the build list
+  local nv kver
+  for nv in "${!DKMS_MODULES[@]}"; do
+    for kver in $(all_kver); do
+      tobuild["$nv/$kver"]=''
+    done
+  done
+  # add modules for new/updated kernels to the build list
+  for kver in "${!KERNEL_VERSIONS[@]}"; do
+    for nv in $(all_nv_from_kver "$kver"); do
+      tobuild["$nv/$kver"]=''
+    done
+  done
+
+  # list of kver which requires depmod refresh
+  local -A depmods=()
+
+  # let's build and install
+  local nvk mod mver
+  local -i retry=1
   while (( $retry > 0 )); do
     retry=0
-    for nvk in "${!DKMS_MODULES[@]}"; do
-      [[ "$nvk" =~ ([^/]+)/([^/]+)/(.+) ]]
+    for nvk in "${!tobuild[@]}"; do
+      [[ "$nvk" =~ ([^/]+)/([^/]+)/(.+) ]] || continue
       mod="${BASH_REMATCH[1]}"
       mver="${BASH_REMATCH[2]}"
       kver="${BASH_REMATCH[3]}"
       # do not build excluded modules
       if ! check_buildexclusive "$mod" "$mver" "$kver"; then
-        unset DKMS_MODULES[$nvk]
+        unset tobuild[$nvk]
         continue
       # skip modules with missing kernel headers
       elif [[ ! -d "$install_tree/$kver/build/include" ]]; then
-        DKMS_MODULES[$nvk]='Missing kernel headers'
+        ERROR_MESSAGES+=("Missing $kver kernel headers for module $mod/$mver.")
+        unset tobuild[$nvk]
         continue
       # skip modules with missing kernel package
       elif [[ ! -d "$install_tree/$kver/kernel" ]]; then
-        DKMS_MODULES[$nvk]='Missing kernel modules tree'
+        ERROR_MESSAGES+=("Missing $kver kernel modules tree for module $mod/$mver.")
+        unset tobuild[$nvk]
         continue
       # postpone modules with missing dependencies
       elif ! check_dependency "$mod" "$mver" "$kver"; then
-        DKMS_MODULES[$nvk]='Missing dependency'
         continue
       fi
       # give it a try dkms
-      run dkms install --no-depmod -m "$mod" -v "$mver" -k "$kver"
-      dmods[$kver]=''
-      unset DKMS_MODULES[$nvk]
+      run dkms install --no-depmod "$mod/$mver" -k "$kver"
+      if (( $? == 0 )); then
+        # register kernel version for later depmod
+        depmods[$kver]=''
+      fi
+      unset tobuild[$nvk]
       # maybe this module was a dep of another, so we retry
       retry=1
     done
   done
   # run depmod later for performance improvments
   if (( $DKMS_DEPMOD )); then
-    for kver in "${!dmods[@]}"; do
+    for kver in "${!depmods[@]}"; do
       run depmod "$kver"
     done
   fi
+  # add errors messages for missing dependencies modules
+  for nvk in "${!tobuild[@]}"; do
+    [[ "$nvk" =~ ([^/]+/[^/]+)/(.+) ]] || continue
+    nv="${BASH_REMATCH[1]}"
+    kver="${BASH_REMATCH[2]}"
+    ERROR_MESSAGES+=("Missing dependencies to install module $nv for kernel $kver.")
+  done
 }
 
-# remove registered modules when built/installed
-# run depmod later for performance improvments
+# remove registered modules
+# run depmod once per kernel for performance improvments
 dkms_remove() {
-  local nvk mod mver kver state
-  local -A dmods=()
-  for nvk in "${!DKMS_MODULES[@]}"; do
-    [[ "$nvk" =~ ([^/]+)/([^/]+)/(.+) ]]
-    mod="${BASH_REMATCH[1]}"
-    mver="${BASH_REMATCH[2]}"
-    kver="${BASH_REMATCH[3]}"
-    # do not remove excluded modules (n.b. display not found errors)
-    if ! check_buildexclusive "$mod" "$mver" "$kver"; then
-      unset DKMS_MODULES[$nvk]
-      continue
-    fi
-    state=$(dkms status -m "$mod" -v "$mver" -k "$kver")
-    if [[ "$state" =~ "$mod/$mver, $kver, "[^:]+": "(added|built|installed) ]]; then
-      dmods[$kver]=''
-      run dkms remove --no-depmod -m "$mod" -v "$mver" -k "$kver"
+  local nv kver
+  local -A depmods=()
+  # remove full modules first
+  for nv in "${!DKMS_MODULES[@]}"; do
+    # try to remove modules one by one to keep the depmod optimization
+    for kver in $(built_kver_from_nv "$nv"); do
+      run dkms remove --no-depmod "$nv" -k "$kver"
       if (( $? == 0 )); then
-        unset DKMS_MODULES[$nvk]
+        # register kernel version for later depmod
+        depmods[$kver]=''
       else
-        DKMS_MODULES[$nvk]='dkms remove failed'
+        ERROR_MESSAGES+=("Failed to remove module $nv for kernel $kver.")
       fi
-    else
-      DKMS_MODULES[$nvk]='Not found in dkms status output'
+    done
+    # ensure module removal (even if only added)
+    if [[ $(dkms status "$nv") ]]; then
+      run dkms remove "$nv"
+      (( $? == 0 )) || ERROR_MESSAGES+=("Failed to remove module $nv.")
     fi
+  done
+  # remove modules for a specific kernel version
+  for kver in "${!KERNEL_VERSIONS[@]}"; do
+    for nv in $(built_nv_from_kver "$kver"); do
+      run dkms remove --no-depmod "$nv" -k "$kver"
+      if (( $? == 0 )); then
+        # register kernel version for later depmod
+        depmods[$kver]=''
+      else
+        ERROR_MESSAGES+=("Failed to remove module $nv for kernel $kver.")
+      fi
+    done
   done
   # run depmod later for performance improvments
   if (( $DKMS_DEPMOD )); then
-    for kver in "${!dmods[@]}"; do
+    for kver in "${!depmods[@]}"; do
       run depmod "$kver"
     done
   fi
-}
-
-# show information about failed modules
-show_errors() {
-  local nvk mod kver
-  for nvk in "${!DKMS_MODULES[@]}"; do
-    mod=${nvk%/*}
-    kver=${nvk##*/}
-    echo "==> Unable to $DKMS_ACTION module $mod for kernel $kver: ${DKMS_MODULES[$nvk]}."
-  done
 }
 
 # display hook usage and exit $1 (default 1)
@@ -191,7 +230,7 @@ main() {
   # prevent each dkms call from failing with authorization errors
   if (( EUID )); then
     echo 'You must be root to use this hook' >&2
-    exit 1
+    return 1
   fi
 
   # parse command line options
@@ -206,10 +245,9 @@ main() {
   shift $((OPTIND - 1))
   (( $# != 1 )) && usage
 
-  # register DKMS action
+  # parse command action to early exit
   case "$1" in
-    install|remove)
-      declare -r DKMS_ACTION="$1";;
+    install|remove) declare -r DKMS_ACTION="$1";;
     *) usage;;
   esac
 
@@ -223,27 +261,41 @@ main() {
   local path
   for path in "$source_tree" "$install_tree"; do
     if [[ ! -d "$path" ]]; then
-      echo "==> Missing mandatory directory: $path. Exiting!"
+      echo "==> Missing mandatory directory: $path. Exiting!" >&2
       return 1
     fi
   done
 
-  # storage for DKMS modules to install/remove
-  # we use associate arrays to prevent duplicate
+  # global storage for changed DKMS modules
+  # we use associate arrays to prevent duplication
+  # the key is <module name>/<module version>/<kernel version>
   declare -A DKMS_MODULES
 
-  # parse stdin paths to guess what do do
+  # global storage for changed linux kernels
+  # we use associate arrays to prevent duplication
+  declare -A KERNEL_VERSIONS
+
+  # global storage for error messages
+  declare -a ERROR_MESSAGES
+
+  # parse stdin paths to guess what we should install/remove
   while read -r path; do
     if [[ "/$path" =~ ^$source_tree/([^/]+)-([^/]+)/dkms\.conf$ ]]; then
-      parse_module "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}"
+      # we match file updates on dkms modules sources
+      DKMS_MODULES["${BASH_REMATCH[1]}/${BASH_REMATCH[2]}"]=''
     elif [[ "/$path" =~ ^$install_tree/([^/]+)/ ]]; then
-      parse_kernel "${BASH_REMATCH[1]}"
+      # we match file updates on kernels install/removal
+      KERNEL_VERSIONS["${BASH_REMATCH[1]}"]=''
     fi
   done
 
   dkms_$DKMS_ACTION
 
-  show_errors
+  # display errors at the end, to maximize readers
+  local msg
+  for msg in "${ERROR_MESSAGES[@]}"; do
+    echo "==> ERROR: $msg" >&2
+  done
 
   return 0
 }
