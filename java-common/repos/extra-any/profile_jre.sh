55f1eea53b772e3777de60e50653bcb71af39277
freswa <freswa@eb2447ed-0c53-47e4-bac8-5bc4a241df78>
2023-01-18 17:34:17 +0000

    archrelease: copy trunk to extra-any
    git-svn-id: file:///srv/repos/svn-packages/svn@466920 eb2447ed-0c53-47e4-bac8-5bc4a241df78

--- java-common/repos/extra-any/profile_jre.sh
+++ java-common/repos/extra-any/profile_jre.sh
@@ -1,3 +1,4 @@
 # Do not change this unless you want to completely by-pass Arch Linux' way
 # of handling Java versions and vendors. Instead, please use script `archlinux-java`
-export PATH=${PATH}:/usr/lib/jvm/default/bin
+append_path '/usr/lib/jvm/default/bin'
+export PATH
